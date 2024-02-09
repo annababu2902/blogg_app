16b685f3046db8b3125aa9aec144054b32019bc5
annababu2902 <annababu2902@gmail.com>
2024-02-09 11:46:08 +0530

    security added

--- controllers/signuprouter.js
+++ controllers/signuprouter.js
@@ -1,17 +1,31 @@
 const express=require("express")
 const signuprouter=require("../models/user")
-
+const bcrypt=require("bcryptjs")
+const user = require("../models/user")
 
 const router=express.Router()
 
-router.post("/signin",async(req,res)=>{
-    let data=req.body
-    let user=new signuprouter(data)
-    let result=await user.save()
+hashedPasswordgenerator=async(pass)=>{
+    const salt=await bcrypt.genSalt(10)
+    return bcrypt.hash(pass,salt)
+}
 
+router.post("/signup",async(req,res)=>{
+ 
+  let{data}={"data":req.body}
+  let password=data.password
+  hashedPasswordgenerator(password).then(
+    (hashedPassword)=>{
+        console.log(hashedPassword);
+        data.password=hashedPassword
+        console.log(data)
+        let blog=new signuprouter(data)
+        let result=blog.save()
     res.json({
         status:"success"
     })
+}
+)
 })
 
 module.exports=router
